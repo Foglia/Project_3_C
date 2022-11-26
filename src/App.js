e92ea27cc31d01abbbdb1fa0a076002681108f39
Flavia Fogliato <flavia.fogliato@gmail.com>
2022-11-26 18:27:22 +0000

    app js update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import axios from "axios";
-import './App.css';
+import './App.css';;
+import AddUserProfile from './components/AddUserProfile.component';
+import Comments from './components/Comments.component.jsx';
+import EditUserProfile from './components/EditUserProfile.component.jsx';
+import Navbar from './components/Navbar.component.jsx';
+import Home from './pages/Home.page';
+import Events from './pages/Events.page';
+import EventDetail from './pages/EventDetail.page';
+import Login from './pages/Login.page';
+import Signup from '.pages/Signup.page';
+import UserProfile from './pages/UserProfile.page'
+
 
 function App() {
   return (
     <div className="App">
-
+    <Home />
     </div>
   );
 }
---
+++ src/pages/Home.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+    <h1>
+        Lisbon EVENTS - A platform to connect and Inform
+    </h1>
+    </div>
+  )
+}
+
+export default Home
