76686882786dcb29d06ef9a6af9701ed0f53b073
Flavia Fogliato <flavia.fogliato@gmail.com>
2022-11-26 20:47:16 +0000

    basic routes e hohme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import './App.css';;
-import AddUserProfile from './components/AddUserProfile.component';
-import Comments from './components/Comments.component.jsx';
-import EditUserProfile from './components/EditUserProfile.component.jsx';
-import Navbar from './components/Navbar.component.jsx';
+import './App.css';
+import { Routes, Route} from 'react-router-dom'
+
+// import AddUserProfile from './components/AddUserProfile.component';
+// import Comments from './components/Comments.component.jsx';
+// import EditUserProfile from './components/EditUserProfile.component.jsx';
+// import Navbar from './components/Navbar.component.jsx';
 import Home from './pages/Home.page';
 import Events from './pages/Events.page';
-import EventDetail from './pages/EventDetail.page';
-import Login from './pages/Login.page';
-import Signup from '.pages/Signup.page';
-import UserProfile from './pages/UserProfile.page'
+// import EventDetail from './pages/EventDetail.page';
+// import Login from './pages/Login.page';
+// import Signup from '.pages/Signup.page';
+// import UserProfile from './pages/UserProfile.page'
 
 
 function App() {
   return (
     <div className="App">
-    <Home />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/events' element={<Events />} />
+    </Routes>
     </div>
   );
 }
